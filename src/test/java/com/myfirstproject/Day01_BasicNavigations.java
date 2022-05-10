7050b0ace5818d46fe938dba644d137a20157ef7
Mehmet Alatas <mehmetualatas@gmail.com>
2022-05-10 23:44:08 +0300

    thats ok

--- src/test/java/com/myfirstproject/Day01_BasicNavigations.java
+++ src/test/java/com/myfirstproject/Day01_BasicNavigations.java
@@ -1,49 +1,31 @@
 package com.myfirstproject;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Day01_BasicNavigations {
-
-//    Create a new class under : BasicNavigations
+    //    Create a new class under : BasicNavigations
 //    Create main method
-
-
-
     public static void main(String[] args) {
         //    Set Path
-        System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
-//    System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");//windows
-
+       // System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
+   System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");//windows
         //    Create chrome driver
         WebDriver driver = new ChromeDriver();
-
         //    Maximize the window
         driver.manage().window().maximize();
-
-
         //    Open google home page https://www.google.com/.
         driver.get("https://www.google.com/");
 //    driver.get("http://www.google.co.uk/");//for UK
-
 //    On the same class, Navigate to amazon home page https://www.amazon.com/
         driver.navigate().to("https://www.amazon.com/");
-
 //    Navigate back to google
         driver.navigate().back();
-
 //    Navigate forward to amazon
-        driver.navigate().forward();
-
-
+      //  driver.navigate().forward();
 //    Refresh the page
         driver.navigate().refresh();
-
 //    Close/Quit the browser
 //    driver.close();
         driver.quit();
-
-
         /*There are 2 ways to go to a page
          * 1. get("URL")
          * 2. navigate().to("URL")
@@ -61,9 +43,5 @@ public class Day01_BasicNavigations {
          * close() --->>>closes ONLY the current active driver
          * quit()---->>>closes ALL browsers that is open during that test run
          * */
-
-
-
     }
-
 }
