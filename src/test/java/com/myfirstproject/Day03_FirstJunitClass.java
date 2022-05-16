0a81afdfb5ba452298abe57e6f7d4aba76acf7c7
Mehmet Alatas <mehmetualatas@gmail.com>
2022-05-16 06:28:43 +0300

    compansated

---
+++ src/test/java/com/myfirstproject/Day03_FirstJunitClass.java
@@ -0,0 +1,62 @@
+package com.myfirstproject;
+import org.junit.*;
+
+public class Day03_FirstJunitClass {
+    /*
+     * *****JUNIT*****
+     * Junit is a testing framework
+     * Testers can use Junit for creating automation framework
+     * Devs can use Junit for creating Unit test framework
+     *
+     * *****ANNOTATIONS******
+     * There are 6 annotations
+     * 1. @Test -> marks a method as a TEST CASE
+     * All test methods are VOID
+     * B/C we create test cases for assertion, not to return a value
+     * We no longer need main method for Creating or Running test cases
+     * We can run test methods form the method level or class level
+     *
+     * 2. @Before : Runs before EACH @Test annotation
+     * 3. @After : Runs after EACH @Test annotation
+     * 4. @BeforeClass : Runs before each class only once
+     * 5. @AfterClass : Runs after each class only once
+     * 6. @Ignore : Skipping a test case
+     *
+     * */
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("beforeClass METHOD");
+    }
+
+    @Before
+    public void setUp(){
+        System.out.println("BEFORE METHOD");
+    }
+    @After
+    public void tearDown(){
+        System.out.println("AFTER METHOD");
+    }
+    @Test
+    public void test1(){
+        System.out.println("This is Test 1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("This is Test 2");
+    }
+    @Ignore
+    @Test
+    public void test3(){
+        System.out.println("This is Test 3");
+    }
+    @Test
+    public void test4(){
+        System.out.println("This is Test 4");
+    }
+
+    @AfterClass
+    public  static void afterClass(){
+        System.out.println("AfterClass METHOD");
+    }
+}
