3bb2b3c79fc438f21d1eae5cb011c3dfe564ffb9
Mehmet Alatas <mehmetualatas@gmail.com>
2022-06-03 09:45:34 +0300

    v2

--- src/test/java/com/myfirstproject/Day13_NoSuchElementException.java
+++ src/test/java/com/myfirstproject/Day13_NoSuchElementException.java
@@ -1,5 +1,5 @@
 package com.myfirstproject;
-import com.myfirstproject.utilities.TestBase;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Before;
@@ -7,34 +7,52 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Day13_NoSuchElementException {
+
     WebDriver driver;
+
     @Before
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
     }
+
     @Test
-    public void noSuchElementExceptionTest(){
+    public void noSuchElementExceptionTest01(){
         driver.get("https://www.carettahotel.com/");
-        driver.findElement(By.linkText("navLogon")).click();//Wrong locator.linkText should be id.NoSuchElementException
+        driver.findElement(By.linkText("Log")).click();//NoSuchElementException
+        //Wrong Locator. Instead of "Log", it should be "Log in"
+        // linkText => partialLinkText
     }
+
+
     @Test
-    public void noSuchElementExceptionTest2(){
+    public void noSuchElementExceptionTest02(){
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
         driver.findElement(By.xpath("//div[@id='start']//button")).click();
-        String helloWorld = driver.findElement(By.xpath("//div[@id='finish']//h4")).getText();
-        Assert.assertEquals("Hello World!",helloWorld);//ComparisonFailure. This is not same as no such element exception
+        String helloWorld = driver.findElement(By.xpath("//div[@id='finish']")).getText();
+        Assert.assertEquals("Hello World!",helloWorld); //ComparisonFailure
+        //Locator is correct, this reason is not same as first test
+        //Wait issue
     }
+
     @Test
-    public void noSuchElementExceptionTest3(){
+    public void noSuchElementExceptionTest03(){
+
         driver.get("https://the-internet.herokuapp.com/dynamic_controls");
-        driver.findElement(By.xpath("(//button[@type='button'])")).click();
+        driver.findElement(By.xpath("(//button[@type='button'])[1]")).click();
         String goneMessage = driver.findElement(By.xpath("//p[@id='message']")).getText();
-        Assert.assertEquals("It's gone!",goneMessage);//NoSuchElementException. The element loads in about 5 second, but driver is so fast
-//        SOLUTION:
-//        Use explicit wait.
-//        Use implicit wait
+        Assert.assertEquals("It's gone!",goneMessage); //NoSuchElementException
+
+//        Solution
+//        We can use implicit wait
+//        We can use explicit wait
+
     }
+
 }
